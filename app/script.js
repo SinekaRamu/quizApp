6f6ba151a2e0948a1e6e1cafb24e4f610ebf9050
SinekaRamu <sineka2001cud@gmail.com>
2023-09-23 17:20:58 +0530

    switch between two screen

--- app/script.js
+++ app/script.js
@@ -1,4 +1,9 @@
-const groupSelect = document.getElementById("selectCategory");
+//Global Variable inialization
+const groupSelect = document.querySelector("#selectCategory");
+const quizCatBox = document.querySelector("#QuizCategory");
+const questionBox = document.querySelector("#Questions");
+const form = document.querySelector("#question-form");
+
 const categories = [
   {
     id: 1,
@@ -8,12 +13,26 @@ const categories = [
   {
     id: 2,
     name: "Electronis",
-    value: "electro",
+    value: "electronics",
   },
   {
     id: 3,
     name: "Fundamental Physics",
     value: "physics",
+  },
+];
+
+const questions = [
+  {
+    id: "cs1",
+    question: "what is brain of computer?",
+    options: [
+      { id: "choice1", text: "CPU", isCorrect: true },
+      { id: "choice2", text: "mouse", isCorrect: false },
+      { id: "choice3", text: "monitor", isCorrect: false },
+      { id: "choice4", text: "ALU", isCorrect: false },
+    ],
+    category: 1,
   },
 ];
 //appends an array of options to a given select element
@@ -33,6 +52,32 @@ appendOptions(groupSelect, categories);
 // Get the selected value and move to the respected page when the button is clicked
 document.getElementById("proceed").addEventListener("click", function (e) {
   e.preventDefault();
-  const selectedValue = groupSelect.value;
-  console.log(selectedValue);
+  clearApp();
+  openQuestions();
 });
+
+function clearApp() {
+  questionBox.innerHTML = " ";
+}
+function openQuestions() {
+  const selectedCat = categories.findIndex(
+    (cat) => cat.value == groupSelect.value
+  );
+  quizCatBox.style.visibility = "collapse";
+  questionBox.style.visibility = "inherit";
+  const cat = categories[selectedCat];
+  questionCategory(cat);
+}
+
+function questionCategory(cat) {
+  const h1 = document.createElement("h1");
+  h1.innerText = cat.name;
+  const backBtn = document.createElement("button");
+  backBtn.innerText = "Go Back";
+  questionBox.append(h1, backBtn);
+  backBtn.addEventListener("click", function () {
+    // e.preventDefault();
+    questionBox.style.visibility = "hidden";
+    quizCatBox.style.visibility = "inherit";
+  });
+}
